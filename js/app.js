88008d1952d04652c9f3d4f94153f2cf1aacbe0f
carlos <carlosleonfranco13@gmail.com>
2023-03-16 23:01:29 -0500

    Añadiendo la alerta en HTML

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     // Asignar eventos
     inputEmail.addEventListener('blur', validar);
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function(){
         if(e.target.value.trim() === ''){
             mostrarAlerta();
         }else{
-            console.log('No esta vacío');
+            console.log('Si hay algo...');
         }
     }
 
@@ -23,8 +24,10 @@ document.addEventListener('DOMContentLoaded', function(){
         // Generar alerta en HTML
         const error = document.createElement('P');
         error.textContent = 'Hubo un error...';
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
 
-        console.log(error);
+        // Inyectar el error al formulario
+        formulario.appendChild(error);
     }
 
 });
