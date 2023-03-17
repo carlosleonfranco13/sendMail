38f9957a94c8a0bbd77c13ab7d4eb6e905c3a8c6
carlos <carlosleonfranco13@gmail.com>
2023-03-16 23:25:42 -0500

    Validando cada campo

--- js/app.js
+++ js/app.js
@@ -14,16 +14,16 @@ document.addEventListener('DOMContentLoaded', function(){
     // Función para validar el formulario
     function validar(e) {
         if(e.target.value.trim() === ''){
-            mostrarAlerta();
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
         }else{
             console.log('Si hay algo...');
         }
     }
 
-    function mostrarAlerta() {
+    function mostrarAlerta(mensaje) {
         // Generar alerta en HTML
         const error = document.createElement('P');
-        error.textContent = 'Hubo un error...';
+        error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
 
         // Inyectar el error al formulario
