fa87527166daac990cc878d4f55938f035fe743d
carlos <carlosleonfranco13@gmail.com>
2023-03-17 20:54:31 -0500

    Añadir spinner

--- js/app.js
+++ js/app.js
@@ -14,12 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const formulario = document.querySelector("#formulario");
   const btnSubmit = document.querySelector('#formulario button[type="submit"]');
   const btnReset = document.querySelector('#formulario button[type="reset"]');
+  const spinner = document.querySelector('#spinner')
 
 
   // Asignar eventos
   inputEmail.addEventListener("input", validar);
   inputAsunto.addEventListener("input", validar);
   inputMensaje.addEventListener("input", validar);
+
+  formulario.addEventListener('submit', enviarEmail);
 
   btnReset.addEventListener('click', function(e) {
     e.preventDefault();
@@ -32,6 +35,13 @@ document.addEventListener("DOMContentLoaded", function () {
     formulario.reset();
     comprobarEmail();
   })
+
+  function enviarEmail(e) {
+    e.preventDefault();
+
+    spinner.classList.add('flex');
+    spinner.classList.remove('hidden');
+  }
 
   // Función para validar el formulario
   function validar(e) {
