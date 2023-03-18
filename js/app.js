298b062c8721eef11cb5149c90a6faefb704f42c
carlos <carlosleonfranco13@gmail.com>
2023-03-17 20:35:25 -0500

    Reset al formulario

--- js/app.js
+++ js/app.js
@@ -13,11 +13,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputMensaje = document.querySelector("#mensaje");
   const formulario = document.querySelector("#formulario");
   const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+  const btnReset = document.querySelector('#formulario button[type="reset"]');
+
 
   // Asignar eventos
   inputEmail.addEventListener("input", validar);
   inputAsunto.addEventListener("input", validar);
   inputMensaje.addEventListener("input", validar);
+
+  btnReset.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // Reiniciar el objeto
+    email.email = '';
+    email.asunto = '';
+    email.mensaje = '';
+
+    formulario.reset();
+    comprobarEmail();
+  })
 
   // Función para validar el formulario
   function validar(e) {
